fc4686d08833773c77b8e27ab5a39f361c5f1f37
foo <foo.avoutar7654@gmail.com>
2020-10-06 01:46:23 +0000

    [feat]create ECR repository

--- vpc.tf
+++ vpc.tf
@@ -7,3 +7,70 @@ resource "aws_subnet" "main" {
   cidr_block = "10.0.1.0/24"
   availability_zone = "ap-northeast-1a"
 }
+
+resource "aws_security_group" "allows_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = [aws_vpc.main.cidr_block]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_vpc_endpoint" "ecr" {
+  vpc_id       = aws_vpc.main.id
+  subnet_ids   = [aws_subnet.main.id]
+  service_name = "com.amazonaws.ap-northeast-1.ecr.dkr"
+  vpc_endpoint_type = "Interface"
+  security_group_ids = [
+    aws_security_group.allows_tls.id
+  ]
+
+  policy = <<EOF
+{
+  "Statement": [
+    {
+      "Action": "*",
+      "Effect": "Allow",
+      "Resource": "*",
+      "Principal": "*"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_vpc_endpoint" "s3" {
+  vpc_id       = aws_vpc.main.id
+  service_name = "com.amazonaws.ap-northeast-1.s3"
+  vpc_endpoint_type = "Gateway"
+
+  policy = <<EOF
+{
+  "Statement": [
+    {
+      "Action": [
+        "s3:GetObject"
+      ],
+      "Effect": "Allow",
+      "Resource": [
+        "arn:aws:s3:::prod-region-starport-layer-bucket/*"
+      ],
+      "Principal": "*"
+    }
+  ]
+}
+  EOF
+}
